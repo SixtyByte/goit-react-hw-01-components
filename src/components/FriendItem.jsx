c49c4dd641c21364dc0677f5e3c3ef54a3f8315b
SixtyByte <valentinvodopyan@ukr.net>
2023-01-19 22:25:42 +0200

    1

--- src/components/FriendItem.jsx
+++ src/components/FriendItem.jsx
@@ -1,7 +1,7 @@
 import '../styles/Friend.css'
 import PropTypes from 'prop-types';
  export const Friend=({friend:{avatar,name,isOnline},})=>{ return <li className="item">
-<span className= {isOnline && "Online"}></span>
+<span className="status"></span>
 <img className="d" src={avatar} alt="User avatar" width="48" />
 <p className="name">{name}</p>
 </li> }
